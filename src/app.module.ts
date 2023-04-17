77dee1a8c2c52fdd98519fff1e2151825c0cab0b
nacir1i <taoufiqnaciri234@gmail.com>
2023-04-17 23:11:52 +0100

    create basic auth controller, service and module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from './auth/auth.module';
+import { AuthService } from './auth/auth.service';
 
 @Module({
-  imports: [],
+  imports: [AuthModule],
   controllers: [],
-  providers: [],
+  providers: [AuthService],
 })
 export class AppModule {}
