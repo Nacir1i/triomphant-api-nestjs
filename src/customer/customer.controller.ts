eb98f0a86bc1a56b2103f64530ee62cfc3101bc8
nacir1i <taoufiqnaciri234@gmail.com>
2023-04-22 14:45:49 +0100

    update prismaService, install swagger

--- src/customer/customer.controller.ts
+++ src/customer/customer.controller.ts
@@ -1,13 +1,28 @@
-import { Controller, Post, NotFoundException, Body } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  NotFoundException,
+  Body,
+  HttpCode,
+  HttpStatus,
+} from '@nestjs/common';
 import { CustomerService } from './customer.service';
 import { CustomerDto } from './dto';
+import { customer } from '@prisma/client';
 
 @Controller('document/customer')
 export class CustomerController {
   constructor(private customerService: CustomerService) {}
 
+  @Post('create')
+  @HttpCode(HttpStatus.OK)
+  async create(@Body() dto: CustomerDto) {
+    return {};
+  }
+
   @Post('getCustomer')
-  async test(@Body() dto: CustomerDto) {
+  @HttpCode(HttpStatus.OK)
+  async getCustomer(@Body() dto: CustomerDto): Promise<customer> {
     const customer = await this.customerService.getCustomer(dto);
 
     if (!customer) {
