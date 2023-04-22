68a21b961c74dea68cabede5cd4198a39387516b
nacir1i <taoufiqnaciri234@gmail.com>
2023-04-22 14:52:04 +0100

    setup swagger docs

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('MyTraiteurApi')
+    .setDescription('Node js API for MyTraiteur developed by Triomphant')
+    .setVersion('2.0')
+    .build();
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
     }),
   );
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
