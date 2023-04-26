3925ba37180e394c858476a425c69ed07a4fa04f
nacir1i <taoufiqnaciri234@gmail.com>
2023-04-26 02:07:46 +0100

    incomplete unit test for au service

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -5,9 +5,13 @@ import { PrismaService } from '../prisma/prisma.service';
 import { ConfigService } from '@nestjs/config';
 import { JwtService } from '@nestjs/jwt';
 import { UserService } from '../user/user.service';
+import { LoginDto } from './dto';
+import { user } from '@prisma/client';
+import { Prisma } from '@prisma/client';
 
 describe('AuthService', () => {
   let service: AuthService;
+  let prismaSerivce: PrismaService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -24,7 +28,39 @@ describe('AuthService', () => {
     service = module.get<AuthService>(AuthController);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  describe('login', () => {
+    it('should return user object with token', async () => {
+      const dto: LoginDto = {
+        username: 'admin',
+        password: 'admin',
+      };
+      const data = {
+        id: 1,
+        username: 'admin',
+        password: 'admin',
+        first_name: 'user',
+        last_name: 'user',
+        image_url: null,
+        created_at: new Date(),
+        recruited_at: new Date(),
+        birth_date: new Date(),
+        salary: 69420,
+        status: null,
+        role_id: 1,
+        contact_information_id: 1,
+        bank_information_id: null,
+        is_deleted: false,
+      } as unknown as Prisma.Prisma__userClient<user>;
+
+      jest
+        .spyOn(prismaSerivce.user, 'findFirst')
+        .mockImplementation(async () => data);
+
+      const result = await service.login(dto);
+
+      expect(result).toEqual(
+        expect.objectContaining({ user: data, token: expect.any(String) }),
+      );
+    });
   });
 });
