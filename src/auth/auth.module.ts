fa343d15b49aca8a9df9c9232d1f5f597427b604
nacir1i <taoufiqnaciri234@gmail.com>
2023-04-26 03:11:25 +0100

    fix UserService relative in app module and create auth e2e tests

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { JwtModule } from '@nestjs/jwt';
-import { UserService } from 'src/user/user.service';
+import { UserService } from '../user/user.service';
 
 @Module({
   imports: [
--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,1 +1,82 @@
+import { INestApplication, ValidationPipe } from '@nestjs/common';
+import { Test } from '@nestjs/testing';
+import * as pactum from 'pactum';
+import { AppModule } from '../src/app.module';
+import { PrismaService } from '../src/prisma/prisma.service';
+import { LoginDto, SignupDto } from '../src/auth/dto';
 
+describe('App e2e testing', () => {
+  let app: INestApplication;
+  let prismaService: PrismaService;
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleRef.createNestApplication();
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+      }),
+    );
+    await app.init();
+    await app.listen(3333);
+
+    prismaService = app.get(PrismaService);
+
+    pactum.request.setBaseUrl('http://localhost:3333');
+  });
+
+  afterAll(() => {
+    app.close();
+  });
+
+  describe('Authentication', () => {
+    describe('login', () => {
+      const loginDto: LoginDto = {
+        username: 'admin',
+        password: 'admin',
+      };
+      const fakeLoginDto: LoginDto = {
+        username: 'adminX',
+        password: 'admin',
+      };
+
+      it('should throw bad request exception', () => {
+        return pactum
+          .spec()
+          .post('/auth/login')
+          .withBody({ password: loginDto.password })
+          .expectStatus(400);
+      });
+      it('should throw bad request exception', () => {
+        return pactum
+          .spec()
+          .post('/auth/login')
+          .withBody({ username: loginDto.username })
+          .expectStatus(400);
+      });
+      it('should throw unauthorized exception', () => {
+        return pactum
+          .spec()
+          .post('/auth/login')
+          .withBody({
+            username: fakeLoginDto.username,
+            password: fakeLoginDto.password,
+          })
+          .expectStatus(401);
+      });
+      it('should login', () => {
+        return pactum
+          .spec()
+          .post('/auth/login')
+          .withBody({
+            username: loginDto.username,
+            password: loginDto.password,
+          })
+          .expectStatus(200);
+      });
+    });
+  });
+});
