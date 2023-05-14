8b322b8b56357473cd881936562b9fb9a9430727
nacir1i <taoufiqnaciri234@gmail.com>
2023-05-15 00:28:15 +0100

    create user controller

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -4,25 +4,58 @@ import {
   Get,
   HttpCode,
   HttpStatus,
-  Post,
+  Query,
+  Param,
+  Patch,
+  Delete,
   NotFoundException,
+  ParseIntPipe,
 } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserDto, PartialTypedUser } from './dto';
+import { ControllerInterface } from 'src/utils/interfaces';
+import { user } from '@prisma/client';
+import { ParseStringPipe } from 'src/utils/customPipes';
 
 @Controller('user')
-export class UserController {
+export class UserController
+  implements ControllerInterface<UserDto, PartialTypedUser, user>
+{
   constructor(private userService: UserService) {}
 
-  @Post('findOne')
+  create(dto: UserDto) {}
+
+  @Get('findOne/:id')
   @HttpCode(HttpStatus.OK)
-  async findOne(@Body() dto: PartialTypedUser) {
-    const user = await this.userService.getUser(dto);
+  async findOne(id: number) {}
 
-    if (!user) {
-      throw new NotFoundException(`User was not found`);
-    }
+  @Get('findOne/:search')
+  @HttpCode(HttpStatus.OK)
+  async findSearch(@Param('search', ParseStringPipe) search: string) {}
 
-    return user;
+  @Get('findAll')
+  @HttpCode(HttpStatus.OK)
+  async findAll() {}
+
+  @Patch('update')
+  @HttpCode(HttpStatus.OK)
+  async update(
+    @Query('id', ParseIntPipe) id: number,
+    @Body() dto: PartialTypedUser,
+  ) {}
+
+  @Get('getPage')
+  @HttpCode(HttpStatus.OK)
+  async getPage(
+    @Query('page', ParseIntPipe) page: number,
+    @Query('limit', ParseIntPipe) limit: number,
+  ) {
+    throw new Error('Method not implemented.');
+  }
+
+  @Delete('delete/:id')
+  @HttpCode(HttpStatus.OK)
+  async delete(@Param('id', ParseIntPipe) id: number) {
+    throw new Error('Method not implemented.');
   }
 }
