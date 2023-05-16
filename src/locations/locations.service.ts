8f8c59b3cb2fa7fa8e7a3d36adfb40f039179cc1
nacir1i <taoufiqnaciri234@gmail.com>
2023-05-16 01:50:00 +0100

    create location service

--- src/locations/locations.service.ts
+++ src/locations/locations.service.ts
@@ -1,4 +1,94 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { ServiceInterface } from 'src/utils/interfaces';
+import { LocationDto, UpdateLocationDto } from './dto';
+import { PrismaService } from '../prisma/prisma.service';
+import { location, Prisma } from '@prisma/client';
 
 @Injectable()
-export class LocationsService {}
+export class LocationsService
+  implements ServiceInterface<LocationDto, UpdateLocationDto, location>
+{
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async create(dto: LocationDto): Promise<location> {
+    return await this.prismaService.location.create({
+      data: {
+        title: dto.title,
+        address: dto.address,
+      },
+    });
+  }
+
+  async findOne(id: number): Promise<location> {
+    return await this.prismaService.location.findFirst({
+      where: {
+        id: id,
+      },
+    });
+  }
+
+  async findSearch(search: string): Promise<[] | location[]> {
+    return await this.prismaService.location.findMany({
+      where: {
+        OR: [
+          {
+            title: {
+              contains: search,
+            },
+          },
+          {
+            address: {
+              contains: search,
+            },
+          },
+        ],
+      },
+    });
+  }
+
+  async findAll(): Promise<[] | location[]> {
+    return await this.prismaService.location.findMany();
+  }
+
+  async getPage(page: number, limit: number): Promise<object> {
+    const startIndex = (page - 1) * limit;
+
+    const locations = await this.prismaService.location.findMany({
+      skip: startIndex,
+      take: limit,
+    });
+
+    const findAll = (await this.findAll()).length;
+
+    const pagesCount = findAll <= limit ? 1 : Math.ceil(findAll / limit);
+    const remainingPages = pagesCount - page >= 0 ? pagesCount - page : 0;
+
+    return { locations, pagesCount, remainingPages };
+  }
+
+  async update(id: number, dto: UpdateLocationDto): Promise<location> {
+    try {
+      return await this.prismaService.location.update({
+        where: {
+          id: id,
+        },
+        data: {
+          title: dto.title,
+          address: dto.address,
+        },
+      });
+    } catch (error) {
+      if (error instanceof Prisma.PrismaClientKnownRequestError) {
+        if (error.code === 'P2025') {
+          throw new NotFoundException(error.meta?.cause);
+        }
+      }
+
+      throw error;
+    }
+  }
+
+  delete(id: number): Promise<location> {
+    throw new Error('Method not implemented.');
+  }
+}
