06f5c5b5b073ada017e13490ff4831b768e28f11
nacir1i <taoufiqnaciri234@gmail.com>
2023-05-16 02:42:51 +0100

    create inventory category controller

--- src/inventory/inventory.controller.ts
+++ src/inventory/inventory.controller.ts
@@ -1,7 +1,80 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Get,
+  Patch,
+  NotFoundException,
+  Body,
+  HttpCode,
+  HttpStatus,
+  ParseIntPipe,
+  Query,
+  Delete,
+  Param,
+} from '@nestjs/common';
+import { ParseStringPipe } from '../utils/customPipes';
 import { InventoryService } from './inventory.service';
+import { ControllerInterface } from 'src/utils/interfaces';
+import { InventoryDto, updateInventoryDto } from './dto';
+import { inventory_category } from '@prisma/client';
+import { UpdateLocationDto } from 'src/locations/dto';
 
-@Controller('inventory')
-export class InventoryController {
+@Controller('category/inventory')
+export class InventoryController
+  implements
+    ControllerInterface<InventoryDto, updateInventoryDto, inventory_category>
+{
   constructor(private readonly inventoryService: InventoryService) {}
+
+  @Post('create')
+  @HttpCode(HttpStatus.CREATED)
+  async create(@Body() dto: InventoryDto) {
+    return await this.inventoryService.create(dto);
+  }
+
+  @Get('findOne/:id')
+  @HttpCode(HttpStatus.OK)
+  async findOne(@Param('id', ParseIntPipe) id: number) {
+    const inventoryCategory = await this.inventoryService.findOne(id);
+
+    if (!inventoryCategory) {
+      throw new NotFoundException(`Inventory #${id} not found`);
+    }
+
+    return inventoryCategory;
+  }
+
+  @Get('findSearch/:search')
+  @HttpCode(HttpStatus.OK)
+  async findSearch(@Param('search', ParseStringPipe) search: string) {
+    return this.inventoryService.findSearch(search);
+  }
+
+  @Get('findAll')
+  @HttpCode(HttpStatus.OK)
+  async findAll() {
+    return this.inventoryService.findAll();
+  }
+
+  @Get('getPage')
+  @HttpCode(HttpStatus.OK)
+  async getPage(
+    @Query('page', ParseIntPipe) page: number,
+    @Query('limit', ParseIntPipe) limit: number,
+  ) {
+    return this.inventoryService.getPage(page, limit);
+  }
+
+  @Patch('update')
+  @HttpCode(HttpStatus.OK)
+  async update(
+    @Query('id', ParseIntPipe) id: number,
+    @Body() dto: UpdateLocationDto,
+  ) {
+    return this.inventoryService.update(id, dto);
+  }
+
+  delete(id: number) {
+    throw new Error('Method not implemented.');
+  }
 }
