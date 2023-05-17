0f29749c86b96a7eaa8a8810016bc0dbcaede87b
nacir1i <taoufiqnaciri234@gmail.com>
2023-05-17 03:14:53 +0100

    finish service controller

--- src/services/services.controller.ts
+++ src/services/services.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Get,
+  Patch,
+  NotFoundException,
+  Body,
+  HttpCode,
+  HttpStatus,
+  ParseIntPipe,
+  Query,
+  Delete,
+  Param,
+} from '@nestjs/common';
+import { ParseStringPipe } from '../utils/customPipes';
 import { ServicesService } from './services.service';
 import { ControllerInterface } from 'src/utils/interfaces';
 import { ServiceDto, UpdateServiceDTO } from './dto';
@@ -10,31 +24,57 @@ export class ServicesController
 {
   constructor(private readonly servicesService: ServicesService) {}
 
-  create(dto: ServiceDto) {
-    throw new Error('Method not implemented.');
+  @Post('create')
+  @HttpCode(HttpStatus.CREATED)
+  async create(@Body() dto: ServiceDto) {
+    return await this.servicesService.create(dto);
   }
 
-  findOne(id: number) {
-    throw new Error('Method not implemented.');
+  @Get('findOne/:id')
+  @HttpCode(HttpStatus.OK)
+  async findOne(@Param('id', ParseIntPipe) id: number) {
+    const service = await this.servicesService.findOne(id);
+
+    if (!service) {
+      throw new NotFoundException(`Service ${id} not found`);
+    }
+
+    return service;
   }
 
-  findSearch(search: string) {
-    throw new Error('Method not implemented.');
+  @Get('findSearch/:search')
+  @HttpCode(HttpStatus.OK)
+  async findSearch(@Param('search', ParseStringPipe) search: string) {
+    return await this.servicesService.findSearch(search);
   }
 
-  findAll() {
-    throw new Error('Method not implemented.');
+  @Get('findAll')
+  @HttpCode(HttpStatus.OK)
+  async findAll() {
+    return await this.servicesService.findAll();
   }
 
-  getPage(page: number, limit: number) {
-    throw new Error('Method not implemented.');
+  @Get('getPage')
+  @HttpCode(HttpStatus.OK)
+  async getPage(
+    @Query('page', ParseIntPipe) page: number,
+    @Query('limit', ParseIntPipe) limit: number,
+  ) {
+    return await this.servicesService.getPage(page, limit);
   }
 
-  update(id: number, dto: UpdateServiceDTO) {
-    throw new Error('Method not implemented.');
+  @Patch('update')
+  @HttpCode(HttpStatus.OK)
+  async update(
+    @Query('id', ParseIntPipe) id: number,
+    @Body() dto: UpdateServiceDTO,
+  ) {
+    return await this.servicesService.update(id, dto);
   }
 
-  delete(id: number) {
-    throw new Error('Method not implemented.');
+  @Delete('delete/:id')
+  @HttpCode(HttpStatus.OK)
+  async delete(@Param('id', ParseIntPipe) id: number) {
+    return await this.servicesService.delete(id);
   }
 }
