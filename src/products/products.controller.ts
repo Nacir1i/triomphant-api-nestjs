dc12e4ca6b21199ec792fb75dc78e246735584a5
nacir1i <taoufiqnaciri234@gmail.com>
2023-05-17 01:56:03 +0100

    finish product skeleton

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,7 +1,40 @@
 import { Controller } from '@nestjs/common';
 import { ProductsService } from './products.service';
+import { ControllerInterface } from 'src/utils/interfaces';
+import { ProductDto, UpdateProductDto } from './dto';
+import { product } from '@prisma/client';
 
 @Controller('products')
-export class ProductsController {
+export class ProductsController
+  implements ControllerInterface<ProductDto, UpdateProductDto, product>
+{
   constructor(private readonly productsService: ProductsService) {}
+
+  create(dto: ProductDto) {
+    throw new Error('Method not implemented.');
+  }
+
+  findOne(id: number) {
+    throw new Error('Method not implemented.');
+  }
+
+  findSearch(search: string) {
+    throw new Error('Method not implemented.');
+  }
+
+  findAll() {
+    throw new Error('Method not implemented.');
+  }
+
+  getPage(page: number, limit: number) {
+    throw new Error('Method not implemented.');
+  }
+
+  update(id: number, dto: UpdateProductDto) {
+    throw new Error('Method not implemented.');
+  }
+
+  delete(id: number) {
+    throw new Error('Method not implemented.');
+  }
 }
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,4 +1,40 @@
 import { Injectable } from '@nestjs/common';
+import { ServiceInterface } from 'src/utils/interfaces';
+import { ProductDto, UpdateProductDto } from './dto';
+import { product } from '@prisma/client';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class ProductsService {}
+export class ProductsService
+  implements ServiceInterface<ProductDto, UpdateProductDto, product>
+{
+  constructor(private readonly prismaService: PrismaService) {}
+
+  create(dto: ProductDto): Promise<product> {
+    throw new Error('Method not implemented.');
+  }
+
+  findOne(id: number): Promise<product> {
+    throw new Error('Method not implemented.');
+  }
+
+  findSearch(search: string): Promise<[] | product[]> {
+    throw new Error('Method not implemented.');
+  }
+
+  findAll(): Promise<[] | product[]> {
+    throw new Error('Method not implemented.');
+  }
+
+  getPage(page: number, limit: number): object {
+    throw new Error('Method not implemented.');
+  }
+
+  update(id: number, dto: UpdateProductDto): Promise<product> {
+    throw new Error('Method not implemented.');
+  }
+
+  delete(id: number): Promise<product> {
+    throw new Error('Method not implemented.');
+  }
+}
