84bd6487e494022231be70d83f160f52ce997b77
nacir1i <taoufiqnaciri234@gmail.com>
2023-05-18 03:07:19 +0100

    create materail controller

--- src/materials/materials.controller.ts
+++ src/materials/materials.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Get,
+  Patch,
+  NotFoundException,
+  Body,
+  HttpCode,
+  HttpStatus,
+  ParseIntPipe,
+  Query,
+  Delete,
+  Param,
+} from '@nestjs/common';
+import { ParseStringPipe } from '../utils/customPipes';
 import { MaterialsService } from './materials.service';
 import { ControllerInterface } from '../utils/interfaces';
 import { MaterialDto, UpdateMaterialDto } from './dto';
@@ -10,31 +24,55 @@ export class MaterialsController
 {
   constructor(private readonly materialsService: MaterialsService) {}
 
-  create(dto: MaterialDto) {
-    throw new Error('Method not implemented.');
+  @Post('create')
+  @HttpCode(HttpStatus.CREATED)
+  async create(@Body() dto: MaterialDto) {
+    return await this.materialsService.create(dto);
   }
 
-  findOne(id: number) {
-    throw new Error('Method not implemented.');
+  @Get('findOne/:id')
+  @HttpCode(HttpStatus.OK)
+  async findOne(@Param('id', ParseIntPipe) id: number) {
+    const material = await this.materialsService.findOne(id);
+
+    if (!material) {
+      throw new NotFoundException(`Material ${id} not found`);
+    }
   }
 
-  findSearch(search: string) {
-    throw new Error('Method not implemented.');
+  @Get('findSearch/:search')
+  @HttpCode(HttpStatus.OK)
+  async findSearch(@Param('search', ParseStringPipe) search: string) {
+    return await this.findSearch(search);
   }
 
-  findAll() {
-    throw new Error('Method not implemented.');
+  @Get('findAll')
+  @HttpCode(HttpStatus.OK)
+  async findAll() {
+    return await this.findAll();
   }
 
-  getPage(page: number, limit: number) {
-    throw new Error('Method not implemented.');
+  @Get('getPage')
+  @HttpCode(HttpStatus.OK)
+  async getPage(
+    @Query('page', ParseIntPipe) page: number,
+    @Query('limit', ParseIntPipe) limit: number,
+  ) {
+    return await this.materialsService.getPage(page, limit);
   }
 
-  update(id: number, dto: UpdateMaterialDto) {
-    throw new Error('Method not implemented.');
+  @Patch('update')
+  @HttpCode(HttpStatus.OK)
+  async update(
+    @Query('id', ParseIntPipe) id: number,
+    @Body() dto: UpdateMaterialDto,
+  ) {
+    return await this.materialsService.update(id, dto);
   }
 
-  delete(id: number) {
-    throw new Error('Method not implemented.');
+  @Delete('delete/:id')
+  @HttpCode(HttpStatus.OK)
+  async delete(@Param('id', ParseIntPipe) id: number) {
+    return await this.materialsService.delete(id);
   }
 }
