c10b039f8f39d786376498e8c4b71fead8442f05
nacir1i <taoufiqnaciri234@gmail.com>
2023-05-19 16:32:16 +0100

    create appointment controller

--- src/appointments/appointments.controller.ts
+++ src/appointments/appointments.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Get,
+  Patch,
+  NotFoundException,
+  Body,
+  HttpCode,
+  HttpStatus,
+  ParseIntPipe,
+  Query,
+  Delete,
+  Param,
+} from '@nestjs/common';
+import { ParseStringPipe } from '../utils/customPipes';
 import { AppointmentsService } from './appointments.service';
 import { ControllerInterface } from 'src/utils/interfaces';
 import { AppointmentDto, UpdateAppointmentDto } from './dto';
@@ -11,31 +25,51 @@ export class AppointmentsController
 {
   constructor(private readonly appointmentsService: AppointmentsService) {}
 
-  create(dto: AppointmentDto) {
-    throw new Error('Method not implemented.');
+  @Post('create')
+  @HttpCode(HttpStatus.CREATED)
+  async create(@Body() dto: AppointmentDto) {
+    return await this.appointmentsService.create(dto);
   }
 
-  findOne(id: number) {
-    throw new Error('Method not implemented.');
+  @Get('findOne/:id')
+  @HttpCode(HttpStatus.OK)
+  async findOne(@Param('id', ParseIntPipe) id: number) {
+    return await this.appointmentsService.findOne(id);
   }
 
-  findSearch(search: string) {
-    throw new Error('Method not implemented.');
+  @Get('findSearch/:search')
+  @HttpCode(HttpStatus.OK)
+  async findSearch(@Param('search', ParseStringPipe) search: string) {
+    return await this.appointmentsService.findSearch(search);
   }
 
-  findAll() {
-    throw new Error('Method not implemented.');
+  @Get('findAll')
+  @HttpCode(HttpStatus.OK)
+  async findAll() {
+    return await this.appointmentsService.findAll();
   }
 
-  getPage(page: number, limit: number) {
-    throw new Error('Method not implemented.');
+  @Get('getPage')
+  @HttpCode(HttpStatus.OK)
+  async getPage(
+    @Query('page', ParseIntPipe) page: number,
+    @Query('limit', ParseIntPipe) limit: number,
+  ) {
+    return await this.appointmentsService.getPage(page, limit);
   }
 
-  update(id: number, dto: UpdateAppointmentDto) {
-    throw new Error('Method not implemented.');
+  @Patch('update')
+  @HttpCode(HttpStatus.OK)
+  async update(
+    @Query('id', ParseIntPipe) id: number,
+    @Body() dto: UpdateAppointmentDto,
+  ) {
+    return await this.appointmentsService.update(id, dto);
   }
 
-  delete(id: number) {
-    throw new Error('Method not implemented.');
+  @Delete('delete/:id')
+  @HttpCode(HttpStatus.OK)
+  async delete(@Param('id', ParseIntPipe) id: number) {
+    return await this.appointmentsService.delete(id);
   }
 }
