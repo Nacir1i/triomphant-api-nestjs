1461aada253d9ed3ea19993aa00f4936ebc33e8f
nacir1i <taoufiqnaciri234@gmail.com>
2023-06-21 21:19:42 +0100

    update controller interface

--- src/utils/interfaces/ControllerInterface.ts
+++ src/utils/interfaces/ControllerInterface.ts
@@ -1,9 +1,9 @@
 export interface ControllerInterface<D, U, P> {
-  create(dto: D): any;
-  findOne(id: number): any;
-  findSearch(search: string): any;
-  findAll(): any;
-  getPage(page: number, limit: number): any;
-  update(id: number, dto: U): any;
-  delete(id: number): any;
+  create(dto: D): Promise<null | P> | void;
+  findOne(id: number): Promise<null | P>;
+  findSearch(search: string): Promise<null | P[]>;
+  findAll(): Promise<[] | P[]>;
+  getPage(page: number, limit: number): Promise<Object> | void;
+  update(id: number, dto: U): Promise<null | P>;
+  delete(id: number): Promise<null | P> | void;
 }
