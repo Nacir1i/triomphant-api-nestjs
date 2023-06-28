3b6bcd8e09924c6af0472f410545eb9a53f4bbe8
nacir1i <taoufiqnaciri234@gmail.com>
2023-06-28 14:23:32 +0100

    create notification controler

--- src/notifications/notifications.controller.ts
+++ src/notifications/notifications.controller.ts
@@ -1,7 +1,35 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  ParseIntPipe,
+  Query,
+} from '@nestjs/common';
 import { NotificationsService } from './notifications.service';
 
 @Controller('notifications')
 export class NotificationsController {
   constructor(private readonly notificationsService: NotificationsService) {}
+
+  @Get('findAll')
+  @HttpCode(HttpStatus.OK)
+  async findAll() {
+    return await this.notificationsService.findAll();
+  }
+
+  @Get('findRelated')
+  @HttpCode(HttpStatus.OK)
+  async findRelated(
+    @Query('role', ParseIntPipe) role: number,
+    @Query('user', ParseIntPipe) user: number,
+  ) {
+    return await this.notificationsService.findRelated(role, user);
+  }
+
+  @Get('findRecent')
+  @HttpCode(HttpStatus.OK)
+  async findRecent() {
+    return await this.notificationsService.findRecent();
+  }
 }
