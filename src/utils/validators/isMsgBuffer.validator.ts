669176179049d944219ba74f453d811df2bc0ed6
nacir1i <taoufiqnaciri234@gmail.com>
2023-07-03 23:09:16 +0100

    install msgpack-lite and create msgpack class validator decorator

---
+++ src/utils/validators/isMsgBuffer.validator.ts
@@ -0,0 +1,46 @@
+import {
+  ValidationArguments,
+  ValidationOptions,
+  registerDecorator,
+} from 'class-validator';
+import * as msgpack from 'msgpack-lite';
+
+export const IS_MSG_BUFFER = 'IsMsgBuffer';
+
+export function isMsgBuffer(value: any): boolean {
+  try {
+    const decodedData = msgpack.decode(value);
+    if (typeof decodedData != 'object') {
+      return false;
+    }
+    return true;
+  } catch (error) {
+    console.log('error', error);
+
+    return false;
+  }
+}
+
+/**
+ * Checks if value is a Buffer
+ */
+export function IsMsgBuffer(
+  validationOptions?: ValidationOptions,
+): PropertyDecorator {
+  return function (object: Object, propertyName: string) {
+    registerDecorator({
+      name: IS_MSG_BUFFER,
+      target: object.constructor,
+      propertyName: propertyName,
+      options: validationOptions,
+      validator: {
+        validate(value: any, args: ValidationArguments) {
+          return isMsgBuffer(value);
+        },
+        defaultMessage(args: ValidationArguments) {
+          return `${args.property} must be a valid Buffer`;
+        },
+      },
+    });
+  };
+}
